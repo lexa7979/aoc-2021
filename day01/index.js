78fd5a08070898f1e1feaeacc90e869e090eb028
Alexander Urban <lexa@toxa.de>
2021-11-24 00:13:01 +0100

    Pre-exercise

---
+++ day01/index.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+
+function inputDataLinesIntegers(filename = "input.txt") {
+  return fs
+    .readFileSync(filename)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((x) => parseInt(x));
+}
+
+function _isPrime(x) {
+  if (x < 3) {
+    return x === 2;
+  }
+  const testStop = Math.floor(Math.sqrt(x));
+  for (let i = 2; i <= testStop; i++) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function _sumOfProductsPrimesWithIndex(list) {
+  return list
+    .map((number, index) => ({ number, index }))
+    .filter(({ number }) => _isPrime(number))
+    .map(({ number, index }) => number * index)
+    .reduce((acc, curr) => acc + curr, 0);
+}
+
+function _sumOfNonPrimesWithAlternatingSigns(list) {
+  return list
+    .map((number, index) => ({ number, index }))
+    .filter(({ number }) => !_isPrime(number))
+    .map(({ number, index }) => (index % 2 === 0 ? number : -number))
+    .reduce((acc, curr) => acc + curr, 0);
+}
+
+function getSolutionPart1() {
+  return _sumOfProductsPrimesWithIndex(inputDataLinesIntegers());
+}
+
+function getSolutionPart2() {
+  return _sumOfNonPrimesWithAlternatingSigns(inputDataLinesIntegers());
+}
+
+console.log("Javascript");
+const part = process.env.part || "part1";
+
+if (part === "part1") {
+  console.log(getSolutionPart1());
+} else {
+  console.log(getSolutionPart2());
+}
+
+module.exports = {
+  _isPrime,
+  _sumOfProductsPrimesWithIndex,
+  _sumOfNonPrimesWithAlternatingSigns,
+  getSolutionPart1,
+  getSolutionPart2,
+  inputDataLinesIntegers,
+};
