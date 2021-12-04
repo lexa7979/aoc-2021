e33af848da19ee619ad362d4276d52994bd21824
Alexander Urban <lexa@toxa.de>
2021-12-04 07:30:08 +0100

    day04: Reduced some lines...

--- day04/index.js
+++ day04/index.js
@@ -21,15 +21,13 @@ function getSolutionPart2() {
 }
 
 function parseBingoSetup(lines) {
+  const _turnIntoNumber = text => parseInt(text, 10);
+  const _handleZeroValues = number => (number === 0 ? 100 : number);
   const result = { draws: null, boards: [] };
   let currentBoard = null;
   lines.forEach(text => {
     if (text.includes(",")) {
-      result.draws = text
-        .trim()
-        .split(",")
-        .map(numberString => parseInt(numberString, 10))
-        .map(number => (number === 0 ? 100 : number));
+      result.draws = text.trim().split(",").map(_turnIntoNumber).map(_handleZeroValues);
       return;
     }
     if (!text.trim()) {
@@ -37,13 +35,7 @@ function parseBingoSetup(lines) {
       return;
     }
     if (currentBoard && /\d+\s+\d+/.test(text)) {
-      currentBoard.push(
-        text
-          .trim()
-          .split(/\s+/)
-          .map(numberString => parseInt(numberString, 10))
-          .map(number => (number === 0 ? 100 : number))
-      );
+      currentBoard.push(text.trim().split(/\s+/).map(_turnIntoNumber).map(_handleZeroValues));
       if (currentBoard.length === 5) {
         result.boards.push(currentBoard);
         currentBoard = null;
@@ -66,27 +58,22 @@ function playBingoAndGetWinningBoardAndNumber({ draws, boards }) {
 }
 
 function playBingoAndGetLosingBoardAndNumber({ draws, boards }) {
+  const _notNull = value => value != null;
   for (let i = 0; i < draws.length; i++) {
     const currentDraw = draws[i];
-    const allIndexesOfStillPlayingBoards = boards
-      .map((item, index) => (item == null ? null : index))
-      .filter(index => index != null);
+    const allIndexesOfStillPlayingBoards = boards.map((item, index) => (item == null ? null : index)).filter(_notNull);
     allIndexesOfStillPlayingBoards.forEach(index => markCurrentDraw({ currentBoard: boards[index], currentDraw }));
     const allIndexesOfJustWinningBoards = boards
-      .map(item => (item == null ? false : checkIfBoardHasWon(item)))
-      .map((hasWon, index) => (hasWon ? index : null))
-      .filter(index => index != null);
+      .map((item, index) => (item && checkIfBoardHasWon(item) ? index : null))
+      .filter(_notNull);
     if (allIndexesOfJustWinningBoards.length === allIndexesOfStillPlayingBoards.length) {
-      return calculateScore({
-        winningBoard: boards[allIndexesOfJustWinningBoards[0]],
-        currentDraw,
-      });
+      return calculateScore({ winningBoard: boards[allIndexesOfJustWinningBoards[0]], currentDraw });
     }
     allIndexesOfJustWinningBoards.forEach(index => {
       boards[index] = null;
     });
   }
-  throw new Error("Nobody lose!");
+  throw new Error("Nobody lost!");
 }
 
 function markCurrentDraw({ currentBoard, currentDraw }) {
@@ -100,23 +87,21 @@ function markCurrentDraw({ currentBoard, currentDraw }) {
 }
 
 function checkIfBoardHasWon(board) {
-  for (let i = 0; i < 5; i++) {
-    if (
+  return [0, 1, 2, 3, 4].some(
+    i =>
       Math.max(board[i][0], board[i][1], board[i][2], board[i][3], board[i][4]) < 0 ||
       Math.max(board[0][i], board[1][i], board[2][i], board[3][i], board[4][i]) < 0 ||
       Math.max(board[0][0], board[1][1], board[2][2], board[3][3], board[4][4]) < 0
-    ) {
-      return true;
-    }
-  }
-  return false;
+  );
 }
 
 function calculateScore({ winningBoard, currentDraw }) {
   let sum = 0;
   winningBoard.forEach(row => {
     row.forEach(number => {
-      sum += number > 0 ? number : 0;
+      if (number > 0) {
+        sum += number;
+      }
     });
   });
   return sum * currentDraw;
